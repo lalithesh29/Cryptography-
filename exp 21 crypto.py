ebbcbca1470c8fdd820b25edbd82cd4430a994f9
lalithesh29 <lalitheshjallarapu29@gmail.com>
2026-08-22 10:52:31 +0530

    Add files via upload

---
+++ exp 21 crypto.py
@@ -0,0 +1,16 @@
+#crypto exp21:
+msg = input("Enter Message: ")
+
+block = 8
+
+padding = block - (len(msg) % block)
+
+if padding == block:
+    padding = block
+
+msg += '1'
+msg += '0' * (padding - 1)
+
+print("Padded Message =", msg)
+
+
